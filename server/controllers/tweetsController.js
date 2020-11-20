8ecb41da14c257e74a8198a848786fa4de7ddbf6
Bibiana Souza <souzabibiana@hotmail.com>
2020-11-20 12:22:27 -0800

    created the post route and tested

--- server/controllers/tweetsController.js
+++ server/controllers/tweetsController.js
@@ -4,6 +4,16 @@ function getAllTweets(_req, res) {
     res.status(200).json(tweets.list());
 }
 
+function postATweet(req,res) {
+    if(req.body.tweet !== undefined) {
+        let request = tweets.postATweet(req.body.tweet);
+        if(request) {
+            res.status(201).send(request);
+        }
+    }
+}
+
 module.exports = {
     getAllTweets,
+    postATweet
 }
