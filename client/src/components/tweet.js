b7c69d360f1e3caca0d060bfbdfb8fc07b36034e
Pattarapond Sukvanachaikul <patt@Pattaraponds-MacBook-Pro.local>
2020-11-20 14:34:14 -0800

    tweet scss

--- client/src/components/tweet.js
+++ client/src/components/tweet.js
@@ -1,14 +1,18 @@
+import '../'
+
 function Tweet(props) {
     // const {tweets} = props.tweets;
     return !props.tweets ? <h2>Loading...</h2> :
     (
-        <ul className="tweet">
-            {props.tweets && props.tweets.map((tweet) => (
-                <li className="tweet__item" key={tweet.id}>
-                    <p>{tweet.tweet} </p>
-                </li>
-            ))}
-        </ul>
+        <div className="tweet">
+            <ul className="tweet__list">
+                {props.tweets && props.tweets.map((tweet) => (
+                    <li className="tweet__item" key={tweet.id}>
+                        <p>{tweet.tweet} </p>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
