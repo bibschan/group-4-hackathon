a22d7d26b1bbca9dc63f22f6b93c1332c163fdf9
7yundao <yunzeng18@gmail.com>
2020-11-20 15:12:38 -0800

    fixed import path

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 import Tweet from './components/tweet';
-import Form from './components/Form';
+import Form from './components/form';
 import './scss/App.css';
 
 class App extends Component {
@@ -16,17 +16,17 @@ class App extends Component {
 
   getTweetData() {
     axios.get(`http://localhost:8080/tweets`)
-    .then((response) => {
-      // console.log(response);
-      const {data} = response;
-      this.setState({
-        tweet: data,
-      });
-      // console.log(this.state.tweet);
-    })
-    .catch((error) => console.log(error));
+      .then((response) => {
+        // console.log(response);
+        const { data } = response;
+        this.setState({
+          tweet: data,
+        });
+        // console.log(this.state.tweet);
+      })
+      .catch((error) => console.log(error));
   }
-  handleClick(data){
+  handleClick(data) {
     axios({
       method: 'post',
       url: 'http://localhost:8080/tweets',
@@ -34,20 +34,20 @@ class App extends Component {
         tweet: data,
       }
     }).then((response) => {
-        const {data} = response;
-        this.setState({
-          id: uuid,
-          tweet: data,
-        });
-        // console.log(this.state.tweet);
-      })
+      const { data } = response;
+      this.setState({
+        id: uuid,
+        tweet: data,
+      });
+      // console.log(this.state.tweet);
+    })
       .catch((error) => console.log(error));
   }
   render() {
 
     return (
       <div className="app">
-        <Form handleClick={this.handleClick}/>
+        <Form handleClick={this.handleClick} />
         <Tweet tweets={this.state.tweet} />
 
       </div>
